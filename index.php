1daa328fce80cec0ee062bceaf55068c2e042993
canzdoankz <canzdoankz@gmail.com>
2021-08-30 11:51:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -64,161 +64,158 @@
     <header></header>
     <div class="page-content">
       <div class="div">
-<div class="ww-home-page" id="home">
-  <div class="ww-wedding-announcement d-flex align-items-center justify-content-start">
-    <div class="container ww-announcement-container">
-      <p class="pt-3" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000">THE WEDDING OF</p><br>
-      <p class="ww-couple-name ww-title" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000">Chandra <br> & <br> Sela</p>
-      <p class="pt-3" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000" data-aos-offset="10">10 | 10 | 2021</p>
-    </div>
-  </div>
-</div>
-<div class="ww-section" id="couple" >
-  <div class="container">
-    <div class="container" style="text-align: center;" >
-      <div class="col-md-12 col-sm-12">
-        <div class="mt-3">
-          <p class="pt-3 " style="color:#bb7944">"Love bears all things, believes all things, hopes all things, endures all things." <br> 1 Corinthians 13:7</p>
-        </div>
-      </div>
-    </div>
-    <br>
-    <br>
-    <br>
-    <div class="col-md-12 col-sm-12">
-      <div class="mt-3">
-        <p class="pt-3 text-muted text-center" >Together with our parents we</p>
-      </div>
-    </div>
-    <div class="row text-center">
-      <div class="col-md-6">
-        <div class="mt-3"><img class="img-fluid" src="images/groom.jpg" alt="Groom" data-aos="flip-left" data-aos-duration="1000"/>
-          <br><br><br>
-          <h3 class="h2 ww-title" style="color:#bb7944">Chandra Buana Putra Angkiriwang</h3>
-          <p class="pt-3 text-muted" style="color:#9c5908" style="text-align: center;">2<sup>nd</sup> son of <br>dr. Adi Supenang <br> Melyanti Mikha Margaretha</p>
-          <div class="ww-social-links"><a class="btn btn-link" href="https://www.facebook.com/chandra.terserah.7/"><i class="fab fa-facebook-f" aria-hidden="true"></i></a><a class="btn btn-link" href="https://www.instagram.com/chandraa_ang/"><i class="fab fa-instagram" aria-hidden="true"></i></a></div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="mt-3"><img class="img-fluid" src="images/bride.jpg" alt="Bride" data-aos="flip-right" data-aos-duration="1000"/>
-          <br><br><br>
-          <h3 class="h2 ww-title" style="color:#bb7944">Valeria Gisela Sepventus Uhing</h3>
-          <p class="pt-3 text-muted" style="color:#9c5908" style="text-align: center;">2<sup>nd</sup> daughter of <br> Donatus Donny Uhing <br> Veronica Marianna de Grave</p>
-          <div class="ww-social-links"><a class="btn btn-link" href="https://www.facebook.com/chela.valeria"><i class="fab fa-facebook-f" aria-hidden="true"></i></a><a class="btn btn-link" href="https://www.instagram.com/valeriagisela/"><i class="fab fa-instagram" aria-hidden="true"></i></a></div>
-        </div>
+  <div class="ww-home-page" id="home">
+    <div class="ww-wedding-announcement d-flex align-items-center justify-content-start">
+      <div class="container ww-announcement-container">
+        <p class="pt-3" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000">THE WEDDING OF</p><br>
+        <p class="ww-couple-name ww-title" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000">Chandra <br> & <br> Sela</p>
+        <p class="pt-3" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000" data-aos-offset="10">10 | 10 | 2021</p>
       </div>
     </div>
   </div>
-</div>
-
-
-<div class="ww-section bg-light" id="events">
-  
-  <div class="container ww-wedding-event" style="text-align: center;">
-    <img class="img-fluid" src="images/BUNGA.png" style="height: auto; width: auto;margin-top: -100px;"/>
+  <div class="ww-section" id="couple" >
     <div class="container">
+      <div class="container" style="text-align: center;" >
+        <div class="col-md-12 col-sm-12">
+          <div class="mt-3">
+            <p class="pt-3 " style="color:#bb7944">"Love bears all things, believes all things, hopes all things, endures all things." <br> 1 Corinthians 13:7</p>
+          </div>
+        </div>
+      </div>
+      <br>
+      <br>
+      <br>
       <div class="col-md-12 col-sm-12">
         <div class="mt-3">
-          <p class="pt-3 text-muted" >Invite you to our wedding</p>
+          <p class="pt-3 text-muted text-center" >Together with our parents we</p>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-6">
+          <div class="mt-3"><img class="img-fluid" src="images/groom.jpg" alt="Groom" data-aos="flip-left" data-aos-duration="1000"/>
+            <br><br><br>
+            <h3 class="h2 ww-title" style="color:#bb7944">Chandra Buana Putra Angkiriwang</h3>
+            <p class="pt-3 text-muted" style="color:#9c5908" style="text-align: center;">2<sup>nd</sup> son of <br>dr. Adi Supenang <br> Melyanti Mikha Margaretha</p>
+            <div class="ww-social-links"><a class="btn btn-link" href="https://www.facebook.com/chandra.terserah.7/"><i class="fab fa-facebook-f" aria-hidden="true"></i></a><a class="btn btn-link" href="https://www.instagram.com/chandraa_ang/"><i class="fab fa-instagram" aria-hidden="true"></i></a></div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="mt-3"><img class="img-fluid" src="images/bride.jpg" alt="Bride" data-aos="flip-right" data-aos-duration="1000"/>
+            <br><br><br>
+            <h3 class="h2 ww-title" style="color:#bb7944">Valeria Gisela Sepventus Uhing</h3>
+            <p class="pt-3 text-muted" style="color:#9c5908" style="text-align: center;">2<sup>nd</sup> daughter of <br> Donatus Donny Uhing <br> Veronica Marianna de Grave</p>
+            <div class="ww-social-links"><a class="btn btn-link" href="https://www.facebook.com/chela.valeria"><i class="fab fa-facebook-f" aria-hidden="true"></i></a><a class="btn btn-link" href="https://www.instagram.com/valeriagisela/"><i class="fab fa-instagram" aria-hidden="true"></i></a></div>
+          </div>
         </div>
       </div>
     </div>
-    <h4 class="pt-3 text-muted">Sunday, 10<sup>th</sup> October 2021</h4>
-    <br>
-    <div class="row">
-      <div class="col-md-4 col-sm-12">
-        <div class="my-3">
-            <ul style="text-align: center;" data-aos="zoom-in-down" data-aos-duration="1000">
-              <li><div class="pt-2" style="color:#bb7944">HOLY MATRIMONY</div></li>
-              <li><a class="pl-2 text-muted btn btn-link" target="_blank" href="https://www.google.com/maps/dir//GEREJA+SANTO+PETRUS+KRISOLOGUS+BSB/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e708a76dc89f07d:0x413acbeaf2fc0bb9?sa=X&ved=2ahUKEwivyp_RpdHyAhWQfH0KHfRdCn0Q9RcwDXoECEwQBA">St. Peter Chrysologus Catholic Church, BSB, Semarang</a></li>
-              <li class="pt-2"><span class="pl-2 text-muted"> 10:00AM - 10:30PM</span></li>
-            </ul>
-        </div>
-      </div>
-      <div class="col-md-4 col-sm-12">
-        <div class="my-3">
-            <ul style="text-align: center;" data-aos="zoom-in-down" data-aos-duration="1000">
-              <li><div class="pt-2" style="color:#bb7944">WEDDING RECEPTION</div></li>
-              <li><a class="pl-2 text-muted btn btn-link" target="_blank" href="https://www.google.com/maps/dir//bsb+lekkers/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e7089f956019327:0x400e262e1ce19e90?sa=X&ved=2ahUKEwiyz-6dptHyAhUlmeYKHV-IDNAQ9RcwGnoECGkQBA">BSB Sport Club and Lakers' Resto, Semarang</a></li>
-              <li class="pt-2"><span class="pl-2 text-muted"> 11:00AM - 13:00PM</span></li>
-            </ul>
-        </div>
-      </div>
-      <div class="col-md-4 col-sm-12">
-        <div class="my-3">
-          
-            <ul style="text-align: center;" data-aos="zoom-in-down" data-aos-duration="1000">
-              <li><div class="pt-2" style="color:#bb7944">LIVE STREAMING</div></li>
-              <li><span class="pl-2 text-muted">Chandra & Sela personal Instagram </span></li>
-              <li class="pt-2"><a class="btn btn-link pl-2 text-muted" target="_blank" href="https://www.instagram.com/valeriagisela/">Join Here</a></li>
-            </ul>
-          
-      </div>
-    </div>
+  </div>
+
+
+  <div class="ww-section bg-light" id="events">
     
-    <div class="container" style="text-align: center;" >
-      <div class="col-md-12 col-sm-12">
-        <div class="mt-3">
-          <p class="pt-3 text-muted">Dear Friends and Family,  <br>
-            We are unable to hold our wedding as originally planned and regretfully must limit our guest list. <br>
-            We wish we could have you with us on our special day, but we feel this is the right decision to ensure the health and safety of our loved ones. <br>
-            We hope you can still celebrate with us by joining via Live Instagram <br>
-            Thank you for you understanding and support. <br> <br>
-            Love,
-        </p>
-        </div>
-        <h3 class="ww-couple-name ww-title" >Chandra & Sela</h3>
-      </div>
-      <img class="img-fluid" src="images/bunga2.png" style="height: auto; width: auto;" />
-    </div>
-    <br>
-  </div>
-</div>
-
-<div class="ww-section bg-white" id="gallery">
-  <div class="ww-photo-gallery">
-    <div class="container">
-      <h2 class="h1 text-center pb-3 ww-title" data-aos="zoom-in-down" data-aos-duration="1000" style="color:#bb7944">Photo Gallery</h2>
-     <div class="ww-gallery" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-duration="1000" data-aos-offset="0">
-        <div class="card-columns">
-          <div class="card"><a href="images/gallery-pic-1.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-1.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-2.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-2.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-3.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-3.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-4.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-4.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-5.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-5.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-6.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-6.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-7.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-7.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-6.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-8.jpg"></a></div>
-          <div class="card"><a href="images/gallery-pic-7.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-9.jpg"></a></div>
+    <div class="container ww-wedding-event" style="text-align: center;">
+      <img class="img-fluid" src="images/BUNGA.png" style="height: auto; width: auto;margin-top: -100px;"/>
+      <div class="container">
+        <div class="col-md-12 col-sm-12">
+          <div class="mt-3">
+            <p class="pt-3 text-muted" >Invite you to our wedding</p>
+          </div>
         </div>
       </div>
+      <h4 class="pt-3 text-muted">Sunday, 10<sup>th</sup> October 2021</h4>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-12">
+          <div class="my-3">
+              <ul style="text-align: center;" data-aos="zoom-in-down" data-aos-duration="1000">
+                <li><div class="pt-2" style="color:#bb7944">HOLY MATRIMONY</div></li>
+                <li><a class="pl-2 text-muted btn btn-link" target="_blank" href="https://www.google.com/maps/dir//GEREJA+SANTO+PETRUS+KRISOLOGUS+BSB/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e708a76dc89f07d:0x413acbeaf2fc0bb9?sa=X&ved=2ahUKEwivyp_RpdHyAhWQfH0KHfRdCn0Q9RcwDXoECEwQBA">St. Peter Chrysologus Catholic Church, BSB, Semarang</a></li>
+                <li class="pt-2"><span class="pl-2 text-muted"> 10:00AM - 10:30PM</span></li>
+              </ul>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <div class="my-3">
+              <ul style="text-align: center;" data-aos="zoom-in-down" data-aos-duration="1000">
+                <li><div class="pt-2" style="color:#bb7944">WEDDING RECEPTION</div></li>
+                <li><a class="pl-2 text-muted btn btn-link" target="_blank" href="https://www.google.com/maps/dir//bsb+lekkers/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e7089f956019327:0x400e262e1ce19e90?sa=X&ved=2ahUKEwiyz-6dptHyAhUlmeYKHV-IDNAQ9RcwGnoECGkQBA">BSB Sport Club and Lakers' Resto, Semarang</a></li>
+                <li class="pt-2"><span class="pl-2 text-muted"> 11:00AM - 13:00PM</span></li>
+              </ul>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <div class="my-3">
+            
+              <ul style="text-align: center;" data-aos="zoom-in-down" data-aos-duration="1000">
+                <li><div class="pt-2" style="color:#bb7944">LIVE STREAMING</div></li>
+                <li><span class="pl-2 text-muted">Chandra & Sela personal Instagram </span></li>
+                <li class="pt-2"><a class="btn btn-link pl-2 text-muted" target="_blank" href="https://www.instagram.com/valeriagisela/">Join Here</a></li>
+              </ul>
+            
+        </div>
+      </div>
+      
+      <div class="container" style="text-align: center;" >
+        <div class="col-md-12 col-sm-12">
+          <div class="mt-3">
+            <p class="pt-3 text-muted">Dear Friends and Family,  <br>
+              We are unable to hold our wedding as originally planned and regretfully must limit our guest list. <br>
+              We wish we could have you with us on our special day, but we feel this is the right decision to ensure the health and safety of our loved ones. <br>
+              We hope you can still celebrate with us by joining via Live Instagram <br>
+              Thank you for you understanding and support. <br> <br>
+              Love,
+          </p>
+          </div>
+          <h3 class="ww-couple-name ww-title" >Chandra & Sela</h3>
+        </div>
+        <img class="img-fluid" src="images/bunga2.png" style="height: auto; width: auto;" />
+      </div>
+      <br>
     </div>
   </div>
-</div>
 
-<div class="ww-section bg-light" id="events">
-  
-  <div class="container ww-wedding-event" style="text-align: center;">
+  <div class="ww-section bg-white" id="gallery">
+    <div class="ww-photo-gallery">
+      <div class="container">
+        <h2 class="h1 text-center pb-3 ww-title" data-aos="zoom-in-down" data-aos-duration="1000" style="color:#bb7944">Photo Gallery</h2>
+      <div class="ww-gallery" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-duration="1000" data-aos-offset="0">
+          <div class="card-columns">
+            <div class="card"><a href="images/gallery-pic-1.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-1.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-2.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-2.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-3.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-3.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-4.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-4.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-5.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-5.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-6.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-6.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-7.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-7.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-6.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-8.jpg"></a></div>
+            <div class="card"><a href="images/gallery-pic-7.jpg" data-toggle="lightbox" data-gallery="ww-gallery"><img class="img-fluid" src="images/gallery-pic-9.jpg"></a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-    <img class="img-fluid" src="images/BUNGA.png" style="height: auto; width: auto;margin-top: -100px;"/>
-    <div class="container" style="text-align: center;" > <br>
-      <div class="col-md-12 col-sm-12">
-        <div class="mt-3">
+  <div class="ww-section bg-light" id="events">
+    <div class="container ww-wedding-event" style="text-align: center;">
+      <img class="img-fluid" src="images/BUNGA.png" style="height: auto; width: auto;margin-top: -100px;"/>
+      <div class="container" style="text-align: center;" > <br> <br> <br>
+        <div class="col-md-12 col-sm-12">
+          <div class="mt-3">
+            <p class="pt-3 text-muted">
+              With all that we have, we’ve been truly blessed. 
+              Your presence and prayers are all that we request. 
+              But if you desire to give nonetheless, we are happy to accepted it.
+          </p>
+          <img class="img-fluid" src="images/bca.PNG" style="height: auto; width: auto;" />
           <p class="pt-3 text-muted">
-            With all that we have, we’ve been truly blessed. 
-            Your presence and prayers are all that we request. 
-            But if you desire to give nonetheless, we are happy to accepted it.
-        </p>
-        <img class="img-fluid" src="images/bca.PNG" style="height: auto; width: auto;" />
-        <p class="pt-3 text-muted">
-          BCA - CHANDRA BUANA P A <br> 4260566852
-        </p>
+            BCA - CHANDRA BUANA P A <br> 4260566852
+          </p>
+          </div>
         </div>
       </div>
-      <img class="img-fluid" src="images/bunga2.png" style="height: auto; width: auto; margin-bottom: -200px;" />
     </div>
   </div>
-</div>
 
 
     <script src="scripts/jquery.min.js?ver=1.1.0"></script>
